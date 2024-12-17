5d72c0aa710088c09ff52c01ef19295775b61149
Taofeek Olusola <olusolataofeek@gmail.com>
2024-12-17 16:01:12 +0100

    final-project

--- db.js
+++ db.js
@@ -1,14 +1,17 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// Use the environment variables for database connection
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT, // Make sure the port is correct
     dialect: 'postgres',
+    logging: false, // Optionally, turn off logging for production
 });
 
 (async () => {
     try {
+        // Test the connection
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
